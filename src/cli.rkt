f9f01a2ded0364bb713470c63276a7fd4ff112e3
Douglas Squirrel <ds@douglassquirrel.com>
2016-10-19 18:55:11 +0100

    use hash to construct parameters

--- src/cli.rkt
+++ src/cli.rkt
@@ -1,11 +1,15 @@
 #lang racket/base
 
-(require racket/cmdline
-         racket/dict)
+(require racket/cmdline)
 
 (provide get-cl-params)
 
+(define default-params
+  (make-hash '()))
+
 (define (get-cl-params)
-  (define defaults '())
+  (define params (hash-copy default-params))
   (command-line
-    #:args (size) (dict-set defaults 'size (string->number size))))
+    #:args (size) (hash-set! params 'size (string->number size)))
+  params
+)
