8c93ba69148fbe786e0e5e7638e26ef2b0db3a6a
Douglas Squirrel <ds@douglassquirrel.com>
2016-10-19 19:28:40 +0100

    collects (but doesn't yet use) tiebreak method parameter

--- src/cli.rkt
+++ src/cli.rkt
@@ -5,7 +5,8 @@
 (provide get-cl-params)
 
 (define default-params
-  (hash 'print-tour #f))
+  (hash 'print-tour #f
+        'tiebreak   'squirrel-cull))
 
 (define (get-cl-params)
   (define params (hash-copy default-params))
@@ -15,5 +16,13 @@
     #:once-each
       (("--print-tour") "Print the squares visited during the computed tour"
         (hash-set! params 'print-tour #t))
-    #:args (size) (hash-set! params 'size (string->number size)))
+      (("--tiebreak") method ("Select tiebreak method. Choices are:"
+                              "'random' for a random selection;"
+                              "'farthest' for the move farthest from centre;"
+                              "'squirrel-cull' for the Squirrel-Cull method; or"
+                              "a permutation of 12345678 for a fixed ordering."
+                              "Squirrel-Cull is the default method.")
+        (hash-set! params 'tiebreak method))
+    #:args (size)
+      (hash-set! params 'size (string->number size)))
   (hash->list params))
--- src/warnsdorff.rkt
+++ src/warnsdorff.rkt
@@ -5,19 +5,23 @@
          "board.rkt"
          "cli.rkt"
          "knight.rkt"
-         "square.rkt"
-         "squirrel-cull.rkt")
+         "square.rkt")
 
 (define cl-params (get-cl-params))
 (define size (dict-ref cl-params 'size))
+(define tiebreak (dict-ref cl-params 'tiebreak))
+
+(printf "Computing tour on square board of size ~a using tiebreak ~a\n"
+        size tiebreak)
 
 (define B (new-board size size (square 1 1)))
 (define T (tour B))
 
-(define num-squares (* size size))
 (if (dict-ref cl-params 'print-tour)
   (displayln (string-join (map square-format T)))
   (void))
+
+(define num-squares (* size size))
 (if (= (length T) num-squares)
   (printf "Successfully visited all ~a squares.\n" num-squares)
   (printf "Failed - visited only ~a of ~a squares.\n" (length T) num-squares))
